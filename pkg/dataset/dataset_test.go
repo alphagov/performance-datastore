45f92dd54744c31d04efde041a6cc4ed8eedc1a0
James Abley <james.abley@gmail.com>
2014-10-17 17:38:03 +0100

    Extract common test setup
    I had a brief play with making DataSet have a pointer to Storage and
    DataSetMetaData. I abandoned that idea since it seemed to read worse,
    with lots of pointer dereferencing and having to copy into temp
    variables when using range to avoid all spawned goroutines pointing to
    the last value of the pointer.
    
    eg
    
    ```
    for _, metaData := range datasets {
    	go checkFreshness(dataset.DataSet{DataSetStorage, metaData}, failing, wg)
    }
    ```
    
    became
    
    ```
    for _, metaData := range datasets {
        copy := metaData
    	go checkFreshness(dataset.DataSet{&DataSetStorage, &copy}, failing, wg)
    }
    ```
    
    I spent a bit of time debugging this. So going for the similar version,
    which doesn’t contain such quirks.

--- pkg/dataset/dataset_test.go
+++ pkg/dataset/dataset_test.go
@@ -17,51 +17,47 @@ func Unmarshal(t string) interface{} {
 }
 
 var _ = Describe("Dataset", func() {
+	var (
+		metaData config_api.DataSetMetaData
+		dataSet  DataSet
+	)
+
+	BeforeEach(func() {
+		metaData = config_api.DataSetMetaData{}
+		dataSet = DataSet{nil, metaData}
+	})
+
 	Describe("DataSetMetaData", func() {
 		It("should default IsPublished to false", func() {
-			metaData := config_api.DataSetMetaData{}
-			dataSet := DataSet{nil, metaData}
 			Expect(dataSet.IsPublished()).Should(BeFalse())
 		})
 
 		It("IsPublished should be read from the meta data", func() {
-			metaData := config_api.DataSetMetaData{}
-			metaData.Published = true
-			dataSet := DataSet{nil, metaData}
+			dataSet.MetaData.Published = true
 			Expect(dataSet.IsPublished()).Should(BeTrue())
 		})
 
 		It("should default IsQueryable to false", func() {
-			metaData := config_api.DataSetMetaData{}
-			dataSet := DataSet{nil, metaData}
 			Expect(dataSet.IsQueryable()).Should(BeFalse())
 		})
 
 		It("IsQueryable should be read from the meta data", func() {
-			metaData := config_api.DataSetMetaData{}
-			metaData.Queryable = true
-			dataSet := DataSet{nil, metaData}
+			dataSet.MetaData.Queryable = true
 			Expect(dataSet.IsQueryable()).Should(BeTrue())
 		})
 
 		It("CappedSize should default to 0", func() {
-			metaData := config_api.DataSetMetaData{}
-			dataSet := DataSet{nil, metaData}
 			Expect(dataSet.CappedSize()).Should(Equal(int64(0)))
 		})
 
 		It("CappedSize should be read from the meta data", func() {
-			metaData := config_api.DataSetMetaData{}
-			metaData.CappedSize = 12345
-			dataSet := DataSet{nil, metaData}
+			dataSet.MetaData.CappedSize = 12345
 			Expect(dataSet.CappedSize()).Should(Equal(int64(12345)))
 		})
 	})
 
 	Describe("Auto IDs", func() {
 		It("Should not alter input when there are no auto IDs defined", func() {
-			metaData := config_api.DataSetMetaData{}
-			dataSet := DataSet{nil, metaData}
 			record := Unmarshal(`{"foo": "foo", "bar": "bar"}`)
 			records := []interface{}{record}
 			actual := dataSet.ProcessAutoIds(records, nil)
@@ -69,9 +65,7 @@ var _ = Describe("Dataset", func() {
 		})
 
 		It("Should add an auto ID based on a single field", func() {
-			metaData := config_api.DataSetMetaData{}
-			metaData.AutoIds = []string{"foo"}
-			dataSet := DataSet{nil, metaData}
+			dataSet.MetaData.AutoIds = []string{"foo"}
 			record := Unmarshal(`{"foo": "foo", "bar": "bar"}`)
 			records := []interface{}{record}
 			actual := dataSet.ProcessAutoIds(records, nil)
@@ -80,22 +74,17 @@ var _ = Describe("Dataset", func() {
 		})
 
 		It("Should add an auto ID based on multiple fields", func() {
-			metaData := config_api.DataSetMetaData{}
-			metaData.AutoIds = []string{"foo", "bar"}
-			dataSet := DataSet{nil, metaData}
+			dataSet.MetaData.AutoIds = []string{"foo", "bar"}
 			record := Unmarshal(`{"foo": "foo", "bar": "bar"}`)
 			records := []interface{}{record}
 			actual := dataSet.ProcessAutoIds(records, nil)
 			expected := Unmarshal(`{"foo": "foo", "bar": "bar","_id": "Zm9vLmJhcg=="}`)
 			Expect([]interface{}{expected}).Should(Equal(actual))
 		})
-
 	})
 
 	Describe("Timestamps", func() {
 		It("Should not modify a record which does not contain a timestamp", func() {
-			metaData := config_api.DataSetMetaData{}
-			dataSet := DataSet{nil, metaData}
 			record := Unmarshal(`{}`)
 			records := []interface{}{record}
 			errors := []error{}
@@ -106,8 +95,6 @@ var _ = Describe("Dataset", func() {
 		})
 
 		It("Should convert valid string timestamp to time.Date", func() {
-			metaData := config_api.DataSetMetaData{}
-			dataSet := DataSet{nil, metaData}
 			record := Unmarshal(`{"_timestamp": "2012-12-12T00:00:00"}`)
 			records := []interface{}{record}
 			errors := []error{}
@@ -118,8 +105,6 @@ var _ = Describe("Dataset", func() {
 		})
 
 		It("Should return an error with an invalid timestamp", func() {
-			metaData := config_api.DataSetMetaData{}
-			dataSet := DataSet{nil, metaData}
 			record := Unmarshal(`{"_timestamp": "invalid"}`)
 			records := []interface{}{record}
 			errors := []error{}
@@ -133,8 +118,6 @@ var _ = Describe("Dataset", func() {
 
 	Describe("Validation", func() {
 		It("Should not allow invalid keys", func() {
-			metaData := config_api.DataSetMetaData{}
-			dataSet := DataSet{nil, metaData}
 			record := Unmarshal(`{"1": "foo"}`)
 			records := []interface{}{record}
 			errors := []error{}
@@ -145,8 +128,6 @@ var _ = Describe("Dataset", func() {
 		})
 
 		It("Should not allow things that look like unreserved keys", func() {
-			metaData := config_api.DataSetMetaData{}
-			dataSet := DataSet{nil, metaData}
 			record := Unmarshal(`{"_foo": "foo"}`)
 			records := []interface{}{record}
 			errors := []error{}
@@ -157,8 +138,6 @@ var _ = Describe("Dataset", func() {
 		})
 
 		It("Should allow things that look like reserved keys", func() {
-			metaData := config_api.DataSetMetaData{}
-			dataSet := DataSet{nil, metaData}
 			record := Unmarshal(`{"_id": "foo"}`)
 			records := []interface{}{record}
 			errors := []error{}
@@ -169,8 +148,6 @@ var _ = Describe("Dataset", func() {
 		})
 
 		It("Should not allow values that aren't whitelisted", func() {
-			metaData := config_api.DataSetMetaData{}
-			dataSet := DataSet{nil, metaData}
 			record := Unmarshal(`{"id": ["foo", "bar"]}`)
 			records := []interface{}{record}
 			errors := []error{}
@@ -181,8 +158,6 @@ var _ = Describe("Dataset", func() {
 		})
 
 		It("Should only allow timestamps that look contain a time.Time instance", func() {
-			metaData := config_api.DataSetMetaData{}
-			dataSet := DataSet{nil, metaData}
 			record := map[string]interface{}{"_timestamp": time.Date(2012, 12, 12, 0, 0, 0, 0, time.UTC)}
 			records := []interface{}{record}
 			errors := []error{}
@@ -193,8 +168,6 @@ var _ = Describe("Dataset", func() {
 		})
 
 		It("Should not allow an int as an _id", func() {
-			metaData := config_api.DataSetMetaData{}
-			dataSet := DataSet{nil, metaData}
 			record := map[string]interface{}{"_id": 1}
 			records := []interface{}{record}
 			errors := []error{}
@@ -205,8 +178,6 @@ var _ = Describe("Dataset", func() {
 		})
 
 		It("Should not allow a string with spaces as an _id", func() {
-			metaData := config_api.DataSetMetaData{}
-			dataSet := DataSet{nil, metaData}
 			record := map[string]interface{}{"_id": "this should fail"}
 			records := []interface{}{record}
 			errors := []error{}
